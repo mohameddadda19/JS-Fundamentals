8c64baac24863aa638b04f010b6131bfe51a7247
mohameddadda19 <40724810+mohameddadda19@users.noreply.github.com>
2025-05-28 20:28:33 +0100

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // Slice off the first two elements (node and script path)
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
